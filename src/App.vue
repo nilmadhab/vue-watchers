202b0d468b00bc6120b81ae92b7ab792b7ec90e0
nmadhab <nilmadhab.mondal@booking.com>
2021-10-31 08:12:10 +0100

    vue components and watchers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Learn Vue </h1>
+  <div class="row">
+    <div class="col-3"></div>
+    <div class="col-6">
+      <select v-model="seletedValue" style="margin-bottom: 20px"
+              class="form-select" aria-label="Default select example"
+              >
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+
+<!--      Square of selected value: {{double}}-->
+
+<!--      display this from Double component-->
+      <DoubleComponent :original="seletedValue" />
+    </div>
+    <div class="col-3"></div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import DoubleComponent from "./components/DoubleComponent";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DoubleComponent
+  },
+  data() {
+    return {
+      seletedValue: 2,
+      double: 2*2
+    }
+  },
+  methods: {
+    computeSquare(){
+      console.log('seletedValue', this.seletedValue);
+      this.double = this.seletedValue * this.seletedValue;
+    }
+  },
+  watch: {
+    seletedValue: function (newValue, oldValue) {
+      console.log("oldValue", oldValue)
+      console.log("newValue", newValue)
+      this.double = newValue*newValue
+    }
   }
 }
 </script>
---
+++ src/components/DoubleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    Square of selected value: {{double}}
+</template>
+<script>
+export default {
+    props: ["original"],
+    data() {
+        return {
+            double: this.original*this.original
+        }
+    },
+    watch: {
+        original: function (newValue) {
+            console.log("new value in component", newValue)
+            this.double = newValue*newValue;
+        }
+    }
+}
+
+</script>
+<style>
+
+</style>
